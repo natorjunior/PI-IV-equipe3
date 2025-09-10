2ec1034293afd191f54f26847a64f1fe44775632
Nator Junior <51802728+natorjunior@users.noreply.github.com>
2025-09-10 16:48:45 +0000

    inserts

--- infra/sql/001_init.sql
+++ infra/sql/001_init.sql
@@ -7,3 +7,8 @@ CREATE TABLE IF NOT EXISTS usuario (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+insert into usuario (nome, email, senha, data_nascimento) values
+('João da Silva', 'joao@example.com', 'senha123', '1990-01-01'),
+('Maria Oliveira', 'maria@example.com', 'senha456', '1985-05-15'),
+('Pedro Santos', 'pedro@example.com', 'senha789', '2000-10-30');
