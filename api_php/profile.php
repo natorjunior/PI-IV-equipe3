af1e266326df28b21ec3e3711a432181223f2200
isaquelevi21 <isaquelevi2021@gmail.com>
2025-12-01 14:20:51 -0300

    Ajuste de Usuário

--- api_php/profile.php
+++ api_php/profile.php
@@ -91,17 +91,13 @@ if ($isOwnProfile) {
             <div class="user-profile" id="userProfile">
                 <img src="<?php echo htmlspecialchars($myAvatar); ?>" alt="User" class="avatar">
                 <div class="user-info">
-                    <span class="username"><?php echo htmlspecialchars($myName); ?></span>
-                    <span class="user-handle">@<?php echo htmlspecialchars($myHandle); ?></span>
+<span class="username">Usuário:<br><?php echo htmlspecialchars($_SESSION['user_name'] ?? 'Usuário'); ?></span>
                 </div>
                 <div class="dropdown" id="dropdownMenu">
                     <a href="profile.php">Meu perfil</a>
-                    <a href="#">Configurações</a>
                     <a href="#" id="logoutLink">Sair</a> 
                 </div>
             </div>
-            <button class="icon-button"><i class="fa fa-cog"></i></button>
-            <button class="icon-button"><i class="fa fa-bell"></i></button>
         </div>
     </header>
 
